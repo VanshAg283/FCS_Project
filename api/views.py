9ee43a0d23b609e1f1964296fb370f5743bec004
Vansh Aggarwal <vanshaggarwal2004@gmail.com>
2025-02-20 03:17:42 +0530

    Fixed issues

--- api/views.py
+++ api/views.py
@@ -2,7 +2,4 @@ from django.http import JsonResponse
 
 def hello_world(request):
     response = JsonResponse({"message": "Hello, World!! Welcome to the Social Media Marketplace!"})
-    # response["Access-Control-Allow-Origin"] = "http://localhost:5173"
-    # response["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
-    # response["Access-Control-Allow-Headers"] = "Content-Type, X-Requested-With"
     return response
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   const [message, setMessage] = useState("");
 
   useEffect(() => {
-    fetch("/api/hello/")
+    fetch("http://192.168.2.251:8000/api/hello/")
       .then((response) => response.json())
       .then((data) => setMessage(data.message))
       .catch((error) => console.error("Error fetching data:", error));
